842943fbd414f5f07c11c402fc5718757c0255bc
sidny <sidny.silva@outlook.com>
2022-08-16 16:36:59 -0300

    fix: data regex

--- src/services/projects/addProject.service.ts
+++ src/services/projects/addProject.service.ts
@@ -1,5 +1,6 @@
-import { ErrorHandler } from "../../helpers/error.helper";
 import { prisma } from "../../app";
+import { ErrorHandler } from "../../helpers/error.helper";
+import { regexDate, regexLink } from "../../utils/regex";
 
 interface Iproject {
   name: string;
@@ -12,33 +13,24 @@ export const addProjectService = async (
   project: Iproject,
   userEmail: string,
 ) => {
-  if (!project.name) {
-    throw new ErrorHandler(400, "Missing project's name");
-  }
-
-  if (!project.description) {
-    throw new ErrorHandler(400, "Missing project's description");
-  }
-
-  if (!project.date) {
-    throw new ErrorHandler(400, "Missing project's creation date");
-  }
-
   if (!project.linkFront && !project.linkBack) {
     throw new ErrorHandler(
       400,
       "Missing project's links, at least one is needed",
     );
   }
-  const regex =
-    /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm;
-  if (project.linkFront != "" && !project.linkFront.match(regex)) {
+
+  if (!project.date.match(regexDate)) {
+    throw new ErrorHandler(400, `Date format DD/MM/YYYY`);
+  }
+
+  if (project.linkFront != "" && !project.linkFront.match(regexLink)) {
     throw new ErrorHandler(
       400,
       "Invalid front link. Ex: https://www.siteName.com/",
     );
   }
-  if (project.linkBack != "" && !project.linkBack.match(regex)) {
+  if (project.linkBack != "" && !project.linkBack.match(regexLink)) {
     throw new ErrorHandler(
       400,
       "Invalid back link. Ex: https://www.siteName.com/",
